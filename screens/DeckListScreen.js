b110b2518991627dd731f7fa5a0564bccbed5db8
Alex Vervloet <alexvervloet@Alexs-MacBook-Pro.local>
2018-03-31 07:18:49 +0800

    Alpha: Shape deck object data to be used by app.

--- screens/DeckListScreen.js
+++ screens/DeckListScreen.js
@@ -1,5 +1,49 @@
 import React, { Component } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+
+const decks = [
+  {
+    0: {
+      name: "React",
+      cardNum: 3,
+      cards: [
+        {
+          question: "Where should you make API calls?",
+          answer: "componentDidMount"
+        },
+        {
+          question: "What is a HOC?",
+          answer:
+            "A High Order Component wraps another component to pass through it."
+        },
+        {
+          question: "How do you get better at React?",
+          answer: "Practice!"
+        }
+      ]
+    }
+  },
+  {
+    1: {
+      name: "React Native",
+      cardNum: 1,
+      cards: [
+        {
+          question: "How do you make a 'div' in React Native?",
+          answer: "<View></View>"
+        }
+      ]
+    }
+  },
+  {
+    2: {
+      name: "My New Deck",
+      cardNum: 0,
+      cards: null
+    }
+  }
+];
+
 class DeckListScreen extends Component {
   render() {
     return (
